0896b5c541c1636523b7f4e6c1e9565d9493c9d4
edgardfn <edgardfinotti@yahoo.com.br>
2022-12-16 15:50:50 -0300

    criado de testes usando jest de criar um novo comentario e enviar notificacao.

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,16 +1,20 @@
-import { Content } from "./content"
+import { Content } from "./content";
 
-test('it should be able to create a notification content',()=> {
-    const content = new Content('Você recebeu uma solicitação de amizadeeeee!');
-
-    expect(content).toBeTruthy();
-});
-
-test('it should not be able to create a notification content with less than 5 characters',()=> {
-    expect(() => new Content('ass')).toThrow();
+describe('Notification content', () => {
+    it('should be able to create a notification content',()=> {
+        const content = new Content('Você recebeu uma solicitação de amizadeeeee!');
+    
+        expect(content).toBeTruthy();
+    });
+    
+    it('should not be able to create a notification content with less than 5 characters',()=> {
+        expect(() => new Content('ass')).toThrow();
+    })
+    
+    it('should not be able to create a notification content with more than 240 characters',()=> {
+        expect(() => new Content('a'.repeat(241))).toThrow();
+    })
 })
 
-test('it should not be able to create a notification content with more than 240 characters',()=> {
-    expect(() => new Content('a'.repeat(241))).toThrow();
-})
+
 
